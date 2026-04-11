9f72ce59c3c9995dca5700b87198baaacb11f39e
bchang12-gif <bchang12@stu.ucc.edu.jm>
2026-04-11 21:12:36 +0000

    Fix UserFactory faker issue

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,14 +19,12 @@ class UserFactory extends Factory
 
     /**
      * Define the model's default state.
-     *
-     * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
