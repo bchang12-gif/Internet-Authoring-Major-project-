9ad4399ed5efd13efc72a2a6fc9b48b734feb802
bchang12-gif <bchang12@stu.ucc.edu.jm>
2026-04-12 20:23:29 -0500

    Update Directory.blade.php

--- resources/views/Directory.blade.php
+++ resources/views/Directory.blade.php
@@ -26,14 +26,14 @@
 <div class="courses-container">
 
     <div class="course">
-        <img src="/images/images.jfif" alt="Bran">
+        <img src="/images/istockphoto-2150163129-612x612.jpg" alt="Jacklin Sterlin">
         <h3> Jack Sterlin </h3>
         <h3 p><a href="tel:1-876-456-8989"> Call </a></p></h3>
         <h3 p><a href="mailto:bchang12@stu.ucc.edu.jm"> Email </a></p></h3>
     </div>
 
     <div class="course">
-        <img src="/images/images.jfif" alt="Taj">
+        <img src="/images/images.jfif" alt="Collen Mclean">
         <h3> Collen Mclean </h3>
         <h3 p><a href="tel:1-876-456-8989"> Call </a></p></h3>
         <h3 p><a href="mailto:tmclean14@stu.ucc.edu.jm"> Email </a></p></h3>
